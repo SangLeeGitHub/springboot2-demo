a09d0bb77107acbda5d76dc0f2f752b2ef0c39fb
Slime@MBP11,2 <hotdeveloper@outlook.com>
2019-06-05 11:33:54 -0400

    1st commit

---
+++ src/main/java/com/example/demo/DemoAppController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class DemoAppController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index.html";
+	}
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		return "Hello World! Test";
+	}
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	@ResponseBody
+	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
